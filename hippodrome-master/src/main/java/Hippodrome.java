e751d83cfe3f5683e563eaca47c4b6e791898864
Alina Hinkul <169094995+AlinaHinkul@users.noreply.github.com>
2024-09-16 22:23:30 +0200

    Done

---
+++ hippodrome-master/src/main/java/Hippodrome.java
@@ -0,0 +1,42 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Objects.isNull;
+
+public class Hippodrome {
+
+    private static final Logger log = LogManager.getLogger(Hippodrome.class);
+
+    private final List<Horse> horses;
+
+    public Hippodrome(List<Horse> horses) {
+        if (isNull(horses)) {
+            log.info("ERROR Hippodrome: Horses list is null");
+            throw new IllegalArgumentException("Horses cannot be null.");
+        } else if (horses.isEmpty()) {
+            log.info("ERROR Hippodrome: Horses list is empty");
+            throw new IllegalArgumentException("Horses cannot be empty.");
+        }
+
+        this.horses = horses;
+        log.info("DEBUG Hippodrome: створення Hippodrome, коней [{}]",horses.size());
+    }
+
+    public List<Horse> getHorses() {
+        return Collections.unmodifiableList(horses);
+    }
+
+    public void move() {
+        horses.forEach(Horse::move);
+    }
+
+    public Horse getWinner() {
+        return horses.stream()
+                .max(Comparator.comparing(Horse::getDistance))
+                .get();
+    }
+}
---
+++ hippodrome-master/src/main/java/Horse.java
@@ -0,0 +1,61 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static java.util.Objects.isNull;
+
+public class Horse {
+
+    private static final Logger log = LogManager.getLogger(Horse.class);
+
+    private final String name;
+    private final double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        if (isNull(name)) {
+            log.info("ERROR Horse: Name is null");
+            throw new IllegalArgumentException("Name cannot be null.");
+        } else if (name.isBlank()) {
+            log.info("ERROR Horse: Name is blank");
+            throw new IllegalArgumentException("Name cannot be blank.");
+        }
+        if (speed < 0) {
+            log.info("ERROR Horse: Speed is negative");
+            throw new IllegalArgumentException("Speed cannot be negative.");
+        }
+        if (distance < 0) {
+            log.info("ERROR Horse: Distance is negative");
+            throw new IllegalArgumentException("Distance cannot be negative.");
+        }
+
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+
+        log.info("DEBUG Horse: створення Horse, ім'я [{}], швидкість [{}]", name, speed);
+    }
+
+    public Horse(String name, double speed) {
+        this(name, speed, 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        distance += speed * getRandomDouble(0.2, 0.9);
+    }
+
+    public static double getRandomDouble(double min, double max) {
+        return (Math.random() * (max - min)) + min;
+    }
+}
